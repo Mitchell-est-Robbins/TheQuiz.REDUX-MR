134959ec96e7f7972a4f4d9a90b97b1350f812c7
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-11-27 19:32:27 -0600

    github downtime commit test. .js pseudo code

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 console.log("winning")
-// GIVEN I am taking a code quiz
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
 
-
+const startBtn =document.querySelector("#startbtn")
 
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
+function quizstart() {
 
+    // starts the quiz
+}
+
+// create an array of questions-----------------
+
+
+// WHEN I answer a question
+// THEN I am presented with another question
+//  click on the answer-------------------------
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// something to handle the time-----------------
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// end of the quiz??----------------------------
+
+// WHEN the game is over
+// THEN I can save my initials and my score
+// score stuff, how to enter via local----------
+
+
+
+startBtn.addEventListener('click', quizstart)
